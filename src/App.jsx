e372c1445c5ca8b7cba39de99f4bbb2fb6998203
james mcfarland <24841376+jamesmcfarland@users.noreply.github.com>
2021-09-30 22:57:46 +0100

    added in theming boilerplate + inital splash page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { createTheme, Typography, ThemeProvider } from "@material-ui/core";
+import { createTheme, Typography, ThemeProvider, CssBaseline } from "@material-ui/core";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SplashScreen from "./pages/splash";
 
 const theme = createTheme({
   typography: {
@@ -11,16 +12,22 @@ const theme = createTheme({
     fontWeightMedium: 500,
     fontWeightBold: 600,
   },
+  palette: {
+    background: {
+      default: "#3a3a3a"
+    }
+  }
 });
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline/>
       <Router>
         <div className="App">
           <Switch>
             <Route exact path="/">
-              <Typography>IT FUCKING WORKS!</Typography>
+              <SplashScreen/>
             </Route>
           </Switch>
         </div>
---
+++ src/pages/splash.jsx
@@ -0,0 +1,18 @@
+import { Box } from "@material-ui/core";
+
+const SplashScreen = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height:"100vh"
+      }}
+    >
+      <div style={{margin:"auto"}}>hello</div>
+    </div>
+  );
+};
+
+export default SplashScreen;
