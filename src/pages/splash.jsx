cfb152a90c2b950dea418c6ac959ed802ab7c09f
james mcfarland <24841376+jamesmcfarland@users.noreply.github.com>
2021-09-30 23:38:09 +0100

    logo looks shite so just using text. added spinner

--- src/pages/splash.jsx
+++ src/pages/splash.jsx
@@ -1,9 +1,15 @@
+import { Box, CircularProgress, Typography } from "@material-ui/core";
 import VerticallyCentered from "../components/verticallyCentered";
+import logo from "../logo.png";
 
 const SplashScreen = () => {
   return (
     <VerticallyCentered>
-      hello!
+      <Box >
+        {/* <img src={logo} style={{maxHeight: "5vh", display:"block", margin: "1rem"}} /> */}
+        <Typography style={{color:"white"}} gutterBottom variant="h4">schedule</Typography>
+        <CircularProgress color="secondary" size="1.5rem"/>
+      </Box>
     </VerticallyCentered>
   );
 };
