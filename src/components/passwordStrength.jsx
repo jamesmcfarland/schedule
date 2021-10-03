60d589ba8e7667d645275922f64317dc82ac33ec
james mcfarland <24841376+jamesmcfarland@users.noreply.github.com>
2021-10-03 23:12:09 +0100

    Created password strength indicator

---
+++ src/components/passwordStrength.jsx
@@ -0,0 +1,7 @@
+const PasswordStrengthIndicator = () => {
+  return <div>I'm a password strength meter</div>;
+};
+
+export default PasswordStrengthIndicator;
+
+//https://upmostly.com/tutorials/build-a-password-strength-meter-react
