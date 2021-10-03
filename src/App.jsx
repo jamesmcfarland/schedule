64fbc403e4114e269d90e1b543db66d5f453b96a
james mcfarland <24841376+jamesmcfarland@users.noreply.github.com>
2021-10-03 18:36:12 +0100

    started working on login page however MUIv5 was just released.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { createTheme, Typography, ThemeProvider, CssBaseline } from "@material-ui/core";
+import {
+  createTheme,
+  Typography,
+  ThemeProvider,
+  CssBaseline,
+} from "@material-ui/core";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SplashScreen from "./pages/splash";
+import Login from "./pages/login";
 
 const theme = createTheme({
   //TODO: Implement full theme and colour pallette
@@ -15,22 +21,25 @@ const theme = createTheme({
   },
   palette: {
     background: {
-      default: "#3a3a3a"
-    }
-  }
+      default: "#3a3a3a",
+    },
+  },
 });
 
 function App() {
   return (
     //TODO: Get firebase setup & connected.
-    
+
     <ThemeProvider theme={theme}>
-      <CssBaseline/>
+      <CssBaseline />
       <Router>
         <div className="App">
           <Switch>
             <Route exact path="/">
-              <SplashScreen/>
+              <SplashScreen />
+            </Route>
+            <Route path="/login">
+              <Login />
             </Route>
           </Switch>
         </div>
---
+++ src/pages/login.jsx
@@ -0,0 +1,30 @@
+import { Box, Card, TextField, Typography } from "@material-ui/core";
+import VerticallyCentered from "../components/verticallyCentered";
+
+const Login = () => {
+  return (
+    <Box className="fancy-bg">
+      <VerticallyCentered>
+        <Card
+          style={{
+            height: "50vh",
+            width: "40vw",
+            backgroundColor: "#3a3a3a",
+            color: "white",
+          }}
+        >
+          <Box padding="2rem" >
+            <Typography variant="h4">Sign in</Typography>
+            <form noValidate autoComplete="off">
+                    <TextField  variant="outlined" label="email address" style={{borderColor:"white"}}>
+
+                    </TextField>
+            </form>
+          </Box>
+        </Card>
+      </VerticallyCentered>
+    </Box>
+  );
+};
+
+export default Login;
