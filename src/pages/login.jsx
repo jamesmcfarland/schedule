469641ad2151c5318127a0f0dbdd1c0aebbb9667
james mcfarland <24841376+jamesmcfarland@users.noreply.github.com>
2021-10-03 22:54:12 +0100

    login page ui done

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,4 +1,13 @@
-import { Box, Card, TextField, Typography, Stack } from "@mui/material";
+import {
+  Box,
+  Card,
+  TextField,
+  Typography,
+  Stack,
+  Button,
+  Grid,
+  Link,
+} from "@mui/material";
 import VerticallyCentered from "../components/verticallyCentered";
 
 const Login = () => {
@@ -14,25 +23,43 @@ const Login = () => {
             // backgroundColor: "#3a3a3a",
           }}
         >
-          <Box padding="2rem">
-            <Typography variant="h4" gutterBottom>Sign in</Typography>
-            <form noValidate autoComplete="off">
-              <Stack spacing={2}>
-                <TextField
-                  
-                  variant="outlined"
-                  type="email"
-                  error={errorText}
-                  helperText={errorText}
-                  label="email address"
-                />
-                <TextField
-                  type="password"
-                  error={errorText}
-                  helperText={errorText}
-                  label="password"
-                />
+          <Box padding="2rem" style={{ height: "100%" }}>
+            <form noValidate autoComplete="off" style={{ height: "100%" }}>
+              <Stack
+                spacing={2}
+                justifyContent="space-between"
+                style={{ height: "100%" }}
+              >
+                <Typography variant="h4" gutterBottom>
+                  Sign in
+                </Typography>
+
+                <Stack spacing={2}>
+                  <TextField
+                    variant="outlined"
+                    type="email"
+                    error={errorText}
+                    helperText={errorText}
+                    label="email address"
+                  />
+                  <TextField
+                    type="password"
+                    error={errorText}
+                    helperText={errorText}
+                    label="password"
+                  />{" "}
+                </Stack>
+                <Button variant="contained" style={{ textTransform: "none" }}>
+                  Sign in
+                </Button>
+                <Typography variant="body2">
+                  haven't got an account yet?{" "}
+                  <Link href="#" underline="none">
+                    sign up
+                  </Link>
+                </Typography>
               </Stack>
+
               {/* TODO: Add in submit button, work on MUIv5 compat styling.  */}
             </form>
           </Box>
