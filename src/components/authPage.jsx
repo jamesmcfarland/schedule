bc604622346eb00a7b3a1dfa3b84db4d000b9d1d
james mcfarland <24841376+jamesmcfarland@users.noreply.github.com>
2021-10-04 14:25:06 +0100

    created authPage.jsx to refactor auth pages background

---
+++ src/components/authPage.jsx
@@ -0,0 +1,44 @@
+import {
+  Box,
+  Card,
+  TextField,
+  Typography,
+  Stack,
+  Button,
+  Link,
+} from "@mui/material";
+import VerticallyCentered from "../components/verticallyCentered";
+
+const AuthPage = ({children}) => {
+  return (
+    <Box className="fancy-bg">
+      <Stack
+        padding="2rem"
+        direction="row"
+        justifyContent="space-between"
+        position="absolute"
+        width="100%"
+      >
+        <Typography variant="h6">schedule</Typography>
+
+        {process.env.NODE_ENV === "development" && (
+          <Typography variant="h6">developer mode</Typography>
+        )}
+      </Stack>
+      <VerticallyCentered>
+        <Card
+          style={{
+            minHeight: "50vh",
+            width: "40vw",
+            borderRadius: "1rem",
+            // backgroundColor: "#3a3a3a",
+          }}
+        >
+          <Box padding="2rem" style={{ height: "100%" }}>{children}</Box>
+        </Card>
+      </VerticallyCentered>
+    </Box>
+  );
+};
+
+export default AuthPage;
