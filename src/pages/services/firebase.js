4701f2f0c3940b3465de98918022b886892af1ba
james mcfarland <24841376+jamesmcfarland@users.noreply.github.com>
2021-10-05 19:19:07 +0100

    added firebase.js config

---
+++ src/pages/services/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const app = initializeApp({
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurememtId,
+});
+
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
+export const analytics = getAnalytics(app);
