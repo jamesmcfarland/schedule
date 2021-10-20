f4ce499f787a161430687eb810fca0f49f5fedab
james mcfarland <24841376+jamesmcfarland@users.noreply.github.com>
2021-10-20 15:49:35 +0100

    structure of the onbaording flow is now setup, added terminology pane also to help explai hwat some of thw wordds mean

--- src/pages/app/onboarding/onboarding.jsx
+++ src/pages/app/onboarding/onboarding.jsx
@@ -1,32 +1,110 @@
-import { Stack, Step, StepLabel, Stepper, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Divider,
+  Grid,
+  Stack,
+  Step,
+  StepLabel,
+  Stepper,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
+import { useState } from "react";
+import TerminologyCard from "./terminology";
 
 const OnboardingFlow = () => {
+  const [activeStep, setactiveStep] = useState(0);
+
+  const renderSwitch = () => {
+    switch (activeStep) {
+      case 0:
+        return <Typography gutterBottom>Step one</Typography>;
+      case 1:
+        return <Typography>Step two</Typography>;
+      case 2:
+        return <Typography>Step three</Typography>;
+      case 3:
+        return <Typography>Step four</Typography>;
+    }
+  };
+
   return (
-    <Box padding="5rem">
-      <Stack spacing={2} alignItems="center">
-      <Typography variant="h6">Welcome to schedule</Typography>
-     <Box  width={700}  >
-     <Stepper activeStep={0}>
-          <Step key={0}>
-              <StepLabel>
-                  Organisation details
-              </StepLabel>
-          </Step>
-          <Step key={1}>
-              <StepLabel>
-                  Departments
-              </StepLabel>
-          </Step>
-          <Step key={2}>
-              <StepLabel>
-                  Test
-              </StepLabel>
-          </Step>
-      </Stepper>
-     </Box>
-      </Stack>
-    </Box>
+    <Container sx={{ minWidth: "95vw" }}>
+      <Grid container alignItems="center">
+        <Grid item xs={2}>
+          <TerminologyCard />
+        </Grid>
+
+        <Grid item xs={8}>
+          <Grid
+            container
+            padding="5rem"
+            direction="column"
+            spacing={4}
+            sx={{ minHeight: "90vh" }}
+          >
+            <Grid item xs={2}>
+              <Stack spacing={2} alignItems="center">
+                <Typography variant="h5">Welcome to schedule</Typography>
+                <Box width={700}>
+                  <Stepper activeStep={activeStep}>
+                    <Step key={0}>
+                      <StepLabel>Organisation details</StepLabel>
+                    </Step>
+                    <Step key={1}>
+                      <StepLabel>Departments</StepLabel>
+                    </Step>
+                    <Step key={2}>
+                      <StepLabel>Test</StepLabel>
+                    </Step>
+                  </Stepper>
+                </Box>
+              </Stack>
+            </Grid>
+            <Grid item xs={8} sx={{ minHeight: "60vh" }}>
+              {renderSwitch()}
+            </Grid>
+
+            <Grid item xs={2}>
+              <Stack
+                spacing={10}
+                direction="row"
+                alignItems="center"
+                justifyContent="center"
+                divider={<Divider orientation="vertical" flexItem />}
+              >
+                <Button
+                  disabled={activeStep === 0}
+                  variant="outlined"
+                  sx={{ textTransform: "none" }}
+                  onClick={() => setactiveStep((prev) => prev - 1)}
+                >
+                  Back
+                </Button>
+                <Button
+                  variant="outlined"
+                  sx={{ textTransform: "none" }}
+                  onClick={() => {
+                    if (activeStep === 2) {
+                      //Do something different here, submit form etc etc.
+                      console.log("done");
+                    } else setactiveStep((prev) => prev + 1);
+                  }}
+                >
+                  {activeStep === 2 ? "Finish" : "Next"}
+                </Button>
+              </Stack>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={2}>
+          {/* {No content} */}
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
---
+++ src/pages/app/onboarding/terminology.jsx
@@ -0,0 +1,30 @@
+import { Card, CardContent, Stack, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
+const TerminologyCard = () => {
+    return (  <Card sx={{ minWidth: "15vw" }}>
+    <CardContent>
+      <Typography gutterBottom variant="h6">
+        Terminology
+      </Typography>
+      <Stack spacing={2}>
+        <Box>
+          <Typography variant="body1">Organisation</Typography>
+          <Typography variant="caption" gutterBottom>
+            A group of departments, it's like a big folder for your
+            companies departments
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="body1">Department</Typography>
+          <Typography variant="caption" gutterBottom>
+            A department, in schedule, is a group of people who work
+            together. Each department has its own rota.
+          </Typography>
+        </Box>
+      </Stack>
+    </CardContent>
+  </Card> );
+}
+ 
+export default TerminologyCard;
