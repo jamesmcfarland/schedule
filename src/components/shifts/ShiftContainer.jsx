e68725f475e0387c990ead0343ddc6333969ef5b
James McFarland <24841376+jamesmcfarland@users.noreply.github.com>
2022-01-23 00:10:56 +0000

    added message for when there are no employees

--- src/components/shifts/ShiftContainer.jsx
+++ src/components/shifts/ShiftContainer.jsx
@@ -1,8 +1,9 @@
 import { DatePicker } from "@mui/lab";
-import { Stack, TextField, Typography } from "@mui/material";
+import { Alert, Link, Stack, TextField, Typography } from "@mui/material";
 import { addDays, format } from "date-fns";
 import _ from "lodash";
 import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import {
   departmentAtom,
@@ -39,8 +40,11 @@ const ShiftContainer = () => {
 
   const { getUserInfoById } = useUser();
 
-  const [selectedStartDate, setSelectedStartDate] = useState(new Date(new Date().setHours(0,0,0,0)));
+  const [selectedStartDate, setSelectedStartDate] = useState(
+    new Date(new Date().setHours(0, 0, 0, 0))
+  );
   const [dayNames, setdayNames] = useState([]);
+  const history = useHistory();
 
   useEffect(() => {
     if (!!organisationId && !!departmentId) {
@@ -97,46 +101,56 @@ const ShiftContainer = () => {
   }, [shift, organisationId, departmentId, selectedStartDate]);
 
   return (
-    <Stack>
-      <Stack direction="row" spacing={2}>
-        <DatePicker
-          label="Start date"
-          value={selectedStartDate}
-          onChange={(newValue) => {
-            setSelectedStartDate(new Date(newValue.setHours(0, 0, 0, 0)));
-          }}
-          renderInput={(params) => <TextField {...params} />}
-        />
-      </Stack>
-      <div className="days-container">
-        {["", ...dayNames].map((el) => {
-          return (
-            <>
-              {el !== "" && (
-                <Stack key={el.name}>
-                  <Typography className="labels">{el.name}</Typography>
-                  <Typography variant="caption">
-                    {format(el.date, "do")}
-                  </Typography>
-                </Stack>
-              )}
-              {el === "" && <p className="labels" key="blnk"></p>}
-            </>
-          );
-        })}
+    <>
+     { !!employees.length && (
+      <Stack>
+        <Stack direction="row" spacing={2}>
+          <DatePicker
+            label="Start date"
+            value={selectedStartDate}
+            onChange={(newValue) => {
+              setSelectedStartDate(new Date(newValue.setHours(0, 0, 0, 0)));
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </Stack>
+        <div className="days-container">
+          {["", ...dayNames].map((el) => {
+            return (
+              <>
+                {el !== "" && (
+                  <Stack key={el.name}>
+                    <Typography className="labels">{el.name}</Typography>
+                    <Typography variant="caption">
+                      {format(el.date, "do")}
+                    </Typography>
+                  </Stack>
+                )}
+                {el === "" && <p className="labels" key="blnk"></p>}
+              </>
+            );
+          })}
 
-        {employees.map((employee) => {
-          return (
-            <GridRow
-              key={employee.employeeId}
-              employee={employee}
-              updated={new Date()}
-              startDate={selectedStartDate}
-            />
-          );
-        })}
-      </div>
-    </Stack>
+          {employees.map((employee) => {
+            return (
+              <GridRow
+                key={employee.employeeId}
+                employee={employee}
+                updated={new Date()}
+                startDate={selectedStartDate}
+              />
+            );
+          })}
+        </div>
+      </Stack>
+      )}
+      {!employees.length && (
+        <Alert severity="warning">
+          There are no employees in this department! Head over to{" "}
+          <Link href="#" onClick={()=>history.push("/app/people")}>people</Link> to add some.
+        </Alert>
+      )}
+    </>
   );
 };
 
